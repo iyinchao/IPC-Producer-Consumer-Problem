385e4009daf1e33972e6dd93f14362c39ff1e75d
Charles Yin <yincharles@163.com>
2013-02-19 11:06:54 +0000

    Initial Commit

---
+++ p-c/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  p-c
+//
+//  Created by Charles Yin on 13-2-19.
+//  Copyright (c) 2013年 Charles Yin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
